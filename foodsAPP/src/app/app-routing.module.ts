af3ed22eccce51fdb905b4a95df07f5b756cb8b6
Kumar Keshav <Kumar.Keshav@godigit.com>
2024-10-17 12:22:47 +0530

    project created

--- foodsAPP/src/app/app-routing.module.ts
+++ foodsAPP/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
+import { RestaurentItemsComponent } from './pages/restaurent-items/restaurent-items.component';
+import { CreateOrdersComponent } from './pages/create-orders/create-orders.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'Login',
+    component: LoginComponent,
+  },{
+    path:'foodCategories',
+    component: CategoriesComponent
+  },
+  {
+    path:'restaurentItems',
+    component: RestaurentItemsComponent
+  },
+  {
+    path:'createorder',
+    component: CreateOrdersComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- foodsAPP/src/app/app.module.ts
+++ foodsAPP/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
+import { RestaurentItemsComponent } from './pages/restaurent-items/restaurent-items.component';
+import { CreateOrdersComponent } from './pages/create-orders/create-orders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CategoriesComponent,
+    RestaurentItemsComponent,
+    CreateOrdersComponent
   ],
   imports: [
     BrowserModule,
